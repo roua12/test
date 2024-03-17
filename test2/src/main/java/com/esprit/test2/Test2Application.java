f4246076f1b4ef102967219685f12205af555880
roua12 <ounissiroua1@gmail.com>
2024-03-17 23:45:28 +0100

    user commit

--- test2/src/main/java/com/esprit/test2/Test2Application.java
+++ test2/src/main/java/com/esprit/test2/Test2Application.java
@@ -8,6 +8,7 @@ public class Test2Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Test2Application.class, args);
+        system.out.println("afcsxus");
     }
 
 }
